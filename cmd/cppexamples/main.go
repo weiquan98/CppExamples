f84e5e0962acf50078f270d64a8f42e57a411eb5
Quan <30767205+weiquan98@users.noreply.github.com>
2025-10-01 18:39:34 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cppexamples/main.go
@@ -0,0 +1,23 @@
+// cmd/cppexamples/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "cppexamples/internal/cppexamples"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := cppexamples.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
